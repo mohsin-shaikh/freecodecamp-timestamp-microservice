c60c5a4601822b92f854ddb281050f67d95b6479
Mohsin Shaikh <mohsinshaikh78601@gmail.com>
2023-03-31 15:58:51 +0530

    Init

---
+++ src/app.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const helmet = require('helmet')
+const ping = require('./api/ping')
+const { port } = require('./config/vars')
+const logger = require('./utils/logger')
+const getTimeStamp = require('./api/getTimeStamp')
+
+
+// Initialize express
+const app = express()
+const PORT = port
+
+// Common Middleware
+app.use(express.json()) // JSON parsing
+app.use(helmet()) // secure apps by setting various HTTP headers
+
+// -----------------------------------------------------------------------------
+// API's
+// -----------------------------------------------------------------------------
+// Health Check
+app.get('/', ping)
+// Create Payment Link
+app.post('/api/:date', getTimeStamp)
+
+// Start express on the defined port
+app.listen(
+    PORT,
+    () => {
+        console.log(`🚀 Server running on port ${PORT}`)
+        logger.info(`Server started and running on ${PORT}`)
+    }
+)
